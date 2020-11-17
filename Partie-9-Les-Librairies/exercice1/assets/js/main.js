ef64647b57264608409003ba25e3de81b4f488eb
FlorianMorel80 <you@example.com>
2020-11-17 16:20:45 +0100

    ex1 ok

---
+++ Partie-9-Les-Librairies/exercice1/assets/js/main.js
@@ -0,0 +1,25 @@
+
+// credit card
+var cleaveCreditCard = new Cleave('.input-credit-card', {
+    creditCard: true,
+    onCreditCardTypeChanged: function (type) {
+    document.getElementById('numberCard').innerHTML= type;
+    }
+});
+
+document.querySelector('.button-credit-card').addEventListener('click', function() {
+    alert(cleaveCreditCard.getRawValue());
+    alert('Achat validé');
+});
+
+// phone
+var cleavePhone = new Cleave('.input-phone', {
+    phone: true,
+    phoneRegionCode: 'FR'
+});
+
+// date
+var cleaveDate = new Cleave('.input-date', {
+    date: true
+});
+
